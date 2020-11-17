4d2b0e745fffadb8a66dd90fbff818419591314b
Allan <amaubert2@myges.fr>
2020-11-17 18:06:36 +0100

    fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,10 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: ['error', 'single'],
+    'semi-style': 'off',
+    'no-extra-semi' : 'off'
   },
   overrides: [
     {
